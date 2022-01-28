2a71a272b2f62de40c62adf2b13ac72d9ae21b78
sergey-25 <123Azxcvbnm>
2022-01-28 14:09:54 +0200

    part of client side is finished and start server side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
-
 import './App.css';
 import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import {ChannelListContainer, ChannelContainer} from './components';
+import {ChannelListContainer, ChannelContainer, Auth} from './components';
 
 const apiKey = 'h5qgf7gbr733';
 
-const client = StreamChat.getInstance(apiKey)
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
 
 function App() {
-  return (
-    <div className="app__wrapper">
-<Chat client={client} theme ="team light">
-  <ChannelListContainer/>
-  <ChannelContainer/>
-</Chat>
-    </div>
-  );
+
+    if (!authToken) return <Auth/>
+
+    return (
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer/>
+            </Chat>
+        </div>
+    );
 }
 
 export default App;
