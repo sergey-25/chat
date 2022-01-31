a31720b265e3680936f8116d8735eaaed9b08ad8
sergey-25 <123Azxcvbnm>
2022-01-31 15:34:13 +0200

    part of client side is finished and start server side

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,51 +1,65 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
-import signinImage from '../assets/signup.jpg';
-import {initialState} from "stream-chat-react/dist/components/Channel/channelState";
 
+import signinImage from '../assets/signup.jpg';
+
+const cookies = new Cookies();
+
+const initialState = {
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+    avatarURL: '',
+}
 
 const Auth = () => {
-
-    const initialState = {
-        fullName: '',
-        username: '',
-        password: '',
-        confirmPassword: '',
-        phoneNumber: '',
-        avatarURL: '',
-    }
-
-
-    const [isSignUp, setIsSignUp] = useState(true);
     const [form, setForm] = useState(initialState);
+    const [isSignup, setIsSignup] = useState(true);
 
     const handleChange = (e) => {
-        setForm({...form, [e.target.name]: e.target.value});
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
 
-        // console.log(form)
-    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    const handleSubmit =(e) =>{
-        e.preventDefault()
-        console.log(form)
-    };
+        const { username, password, phoneNumber, avatarURL } = form;
+
+        const URL = 'https://localhost:5000/auth';
+        // const URL = 'https://medical-pager.herokuapp.com/auth';
+
+        const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName: form.fullName, phoneNumber, avatarURL,
+        });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
+    }
 
     const switchMode = () => {
-        setIsSignUp((prevIsSignUp) => !prevIsSignUp)
-    };
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+    }
 
     return (
         <div className="auth__form-container">
             <div className="auth__form-container_fields">
                 <div className="auth__form-container_fields-content">
-                    <p>
-                        {isSignUp ? 'Sign Up' : 'Sign In'}
-                    </p>
-                    <form
-                        onSubmit={handleSubmit}
-                    >
-                        {isSignUp && (
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit={handleSubmit}>
+                        {isSignup && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="fullName">Full Name</label>
                                 <input
@@ -67,7 +81,7 @@ const Auth = () => {
                                 required
                             />
                         </div>
-                        {isSignUp && (
+                        {isSignup && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="phoneNumber">Phone Number</label>
                                 <input
@@ -79,7 +93,7 @@ const Auth = () => {
                                 />
                             </div>
                         )}
-                        {isSignUp && (
+                        {isSignup && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="avatarURL">Avatar URL</label>
                                 <input
@@ -101,7 +115,7 @@ const Auth = () => {
                                 required
                             />
                         </div>
-                        {isSignUp && (
+                        {isSignup && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="confirmPassword">Confirm Password</label>
                                 <input
@@ -114,27 +128,27 @@ const Auth = () => {
                             </div>
                         )}
                         <div className="auth__form-container_fields-content_button">
-                                <button>{isSignUp ? 'Sign Up' : 'Sign In'}</button>
+                            <button>{isSignup ? "Sign Up" : "Sign In"}</button>
                         </div>
                     </form>
                     <div className="auth__form-container_fields-account">
                         <p>
-                            {isSignUp
-                                ? 'Already have an account '
-                                : 'Don`t have an account?'
+                            {isSignup
+                                ? "Already have an account?"
+                                : "Don't have an account?"
                             }
                             <span onClick={switchMode}>
-                                    {isSignUp ? ' Sign In' : 'Sign Up'}
-                                </span>
+                             {isSignup ? 'Sign In' : 'Sign Up'}
+                             </span>
                         </p>
                     </div>
                 </div>
             </div>
             <div className="auth__form-container_image">
-                <img src={signinImage} alt="sign in"/>
+                <img src={signinImage} alt="sign in" />
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Auth;
+export default Auth
--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -1,41 +1,46 @@
 import React from 'react';
-import {addChannel} from '../assets'
 
-const TeamChannelList = ({children, error = false, loading, type}) => {
+import { AddChannel } from '../assets';
 
-    if (error) {
-        return type === 'team'
-            ?
-            (<div className="team-channel-list">
+const TeamChannelList = ({ setToggleContainer, children, error = false, loading, type, isCreating, setIsCreating, setCreateType, setIsEditing }) => {
+    if(error) {
+        return type === 'team' ? (
+            <div className="team-channel-list">
                 <p className="team-channel-list__message">
                     Connection error, please wait a moment and try again.
                 </p>
-            </div>)
-            :
-            null;
+            </div>
+        ) : null
     }
 
-
-    if (loading) {
+    if(loading) {
         return (
             <div className="team-channel-list">
                 <p className="team-channel-list__message loading">
-                    {type === 'team' ? 'Channels' : 'Messages'} Loading...
+                    {type === 'team' ? 'Channels' : 'Messages'} loading...
                 </p>
             </div>
         )
     }
+
     return (
         <div className="team-channel-list">
             <div className="team-channel-list__header">
                 <p className="team-channel-list__header__title">
                     {type === 'team' ? 'Channels' : 'Direct Messages'}
                 </p>
-            {/*    Button - add channel*/}
+                <AddChannel
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
+                    type={type === 'team' ? 'team' : 'messaging'}
+                    setToggleContainer={setToggleContainer}
+                />
             </div>
             {children}
         </div>
-    );
-};
+    )
+}
 
-export default TeamChannelList;
+export default TeamChannelList
